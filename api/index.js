6a30b6d4605236610b8d09b654b0bc7af1623d3f
hassannoor230 <hassannoor2309@gmail.com>
2026-09-08 00:36:51 +0500

    Fix Vercel API route prefix handling

--- api/index.js
+++ api/index.js
@@ -1,1 +1,8 @@
-module.exports = require('../server');
+const app = require('../server');
+
+module.exports = (req, res, next) => {
+	if (req.url && !req.url.startsWith('/api')) {
+		req.url = `/api${req.url.startsWith('/') ? '' : '/'}${req.url}`;
+	}
+	return app(req, res, next);
+};
